2fe347b08d488fcf4b2ea5b6729e218459d91e9b
avery <sheelyavery@gmail.com>
2025-12-17 19:14:05 -0500

    (just temp prob wont compile) upload a BETTER skeleton one that isnt stupid

--- src/main/java/com/myname/mymodid/ClientProxy.java
+++ src/main/java/com/myname/mymodid/ClientProxy.java
@@ -11,6 +11,7 @@ public class ClientProxy extends CommonProxy {
     public void init(FMLInitializationEvent event) {
         super.init(event);
         Keybinds.register();
+        // testing git cli commit
     }
 
 }
