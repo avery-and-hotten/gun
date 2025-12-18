bb85ac70bfa488d14ec9c156ec8acb96af844591
avery <sheelyavery@gmail.com>
2025-12-17 19:02:32 -0500

    (just temp prob wont compile) upload a BETTER skeleton one that isnt stupid

--- src/main/java/com/myname/mymodid/Keybinds.java
+++ src/main/java/com/myname/mymodid/Keybinds.java
@@ -1,34 +1,60 @@
 package com.myname.mymodid;
+import cpw.mods.fml.common.eventhandler.EventPriority;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import org.lwjgl.input.Keyboard;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.client.settings.KeyBinding;
 
 public class Keybinds {
-    public static KeyBinding crouchSlideKey;
-    public static KeyBinding reloadKey;
-    public static KeyBinding weaponModeKey;
-    public static KeyBinding dolphinDiveKey;
-    public static KeyBinding changeSightKey;
+    public static final KeyBinding crouchSlideKey = new KeyBinding("key.mymod.crouchslide", Keyboard.KEY_C, "movement controls");
+    public static final KeyBinding reloadKey = new KeyBinding("key.mymod.reload", Keyboard.KEY_R, "movement controls");
+    public static final KeyBinding weaponModeKey = new KeyBinding("key.mymod.weaponmode", Keyboard.KEY_V, "movement controls");
+    public static final KeyBinding dolphinDiveKey = new KeyBinding("key.mymod.dolphindive", Keyboard.KEY_X, "movement controls");
+    public static final KeyBinding changeSightKey = new KeyBinding("key.mymod.changesight", Keyboard.KEY_T, "movement controls");
 
+    public static KeyBinding[] keybinds;
     // use codechickenlib for network io and possibly obj rendering if taking it from hbm doesnt work out
     // remeber to use jni to link to libflextrace so we can profile ourselves
 
     public static void register() {
-        // make new keybin
-        reloadKey = new KeyBinding("key.mymod.reload", Keyboard.KEY_R, "movement controls");
-        ClientRegistry.registerKeyBinding(reloadKey);
+        // register ts keybinds
+        keybinds[0] = crouchSlideKey;
+        keybinds[1] = reloadKey;
+        keybinds[2] = weaponModeKey;
+        keybinds[3] = dolphinDiveKey;
+        keybinds[4] = changeSightKey;
 
-        weaponModeKey = new KeyBinding("key.mymod.weaponmode", Keyboard.KEY_V, "movement controls");
-        ClientRegistry.registerKeyBinding(weaponModeKey);
+        for (KeyBinding key: keybinds) {
+            ClientRegistry.registerKeyBinding(key);
+        }
+    }
 
-        dolphinDiveKey = new KeyBinding("key.mymod.dolphindive", Keyboard.KEY_X, "movement controls");
-        ClientRegistry.registerKeyBinding(dolphinDiveKey);
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent(priority= EventPriority.NORMAL, receiveCanceled=true)
+    public static void onEvent(InputEvent.KeyInputEvent event) {
+        //debug
+        System.out.println("DolphinDive key event: " + event);
 
-        crouchSlideKey = new KeyBinding("key.mymod.crouchslide", Keyboard.KEY_C, "movement controls");
-        ClientRegistry.registerKeyBinding(crouchSlideKey);
+        for (KeyBinding key: keybinds) {
+            if (key.isPressed()) {
+                switch (key.getKeyDescription()) {
+                    case "key.mymod.crouchslide":
+                        //
+                    case "key.mymod.reload":
+                        //
+                    case "key.mymod.weaponmode":
+                        //
+                    case "key.mymod.dolphindive":
+                        //
+                    case "key.mymod.changesight":
+                        //
+                }
+            }
 
-        changeSightKey = new KeyBinding("key.mymod.changesight", Keyboard.KEY_T, "movement controls");
-        ClientRegistry.registerKeyBinding(changeSightKey);
+        }
 
     }
 }
